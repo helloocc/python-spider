7121f6572b8d7fb86d4f66667627f0bc5bb9bc57
helloocc <xixizxx@gmail.com>
2016-06-16 15:20:08 +0800

    handle exception.

--- dianping/main.py
+++ dianping/main.py
@@ -3,6 +3,7 @@ import time
 from spider import Spider
 from sender import Sender
 from collections import OrderedDict
+from __builtin__ import str
 
 def check(new_data):
     file_data = OrderedDict()
@@ -34,22 +35,26 @@ if __name__ == "__main__":
     url = "http://s.dianping.com/event/nanjing/c1" 
 
     while True:
-        hour = int(time.strftime('%H'))
-        print time.strftime('%Y%m%d %H:%M:%S')
-        
-        if hour >= 9 and hour <= 18: 
-            new_data = obj_spider.craw(url)
-            add_items = check(new_data)
-            if(len(add_items) > 0):
-                print "New items!!!"
-                title = time.strftime('%Y%m%d %H:%M:%S')
-                content = "\n".join(add_items.values())
-                obj_sender.send_email(title, content)
+        try:
+            hour = int(time.strftime('%H'))
+            print time.strftime('%Y%m%d %H:%M:%S')
+            
+            if hour >= 9 and hour <= 18: 
+                new_data = obj_spider.craw(url)
+                add_items = check(new_data)
+                if(len(add_items) > 0):
+                    print "New items!!!"
+                    title = time.strftime('%Y%m%d %H:%M:%S') + '--ubuntu'
+                    content = "\n".join(add_items.values())
+                    obj_sender.send_email(title, content)
+                else:
+                    print "No change."
+                time.sleep(20)
+                 
+            elif hour < 8 or hour > 20:
+                time.sleep(3598)
             else:
-                print "No change."
-            time.sleep(20)
-             
-        elif hour < 8 or hour > 20:
-            time.sleep(3598)
-        else:
-            time.sleep(1000)
+                time.sleep(1000)
+        except Exception, e:
+            print str(e);
+        
