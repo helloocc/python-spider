a81877a37541d49d63ace95dd1de9a72fbb2c020
helloocc <xixizxx@gmail.com>
2017-08-27 17:58:57 +0800

    add url.

--- lianjia/main.py
+++ lianjia/main.py
@@ -41,14 +41,16 @@ def check(new_data):
 if __name__ == "__main__":
     obj_spider = Spider()
     obj_sender = Sender()
-    url = "https://hz.lianjia.com/ershoufang/rs%E5%8D%97%E5%B2%B8%E6%99%B6%E9%83%BD%E8%8A%B1%E5%9B%AD/"
+    url1 = "https://hz.lianjia.com/ershoufang/rs%E5%8D%97%E5%B2%B8%E6%99%B6%E9%83%BD%E8%8A%B1%E5%9B%AD/"
+    url2 = "https://hz.lianjia.com/ershoufang/rs%E9%A3%8E%E6%99%AF%E8%9D%B6%E9%99%A2/"
     while True:
         try:
             hour = int(time.strftime('%H'))
             print time.strftime('%Y%m%d %H:%M:%S')
 
             if 0 < hour < 24:
-                new_data = obj_spider.craw(url)
+                new_data = obj_spider.craw(url1)
+                new_data.extend(obj_spider.craw(url2))
                 add_items = check(new_data)
                 if len(add_items) > 0:
                     print "New items!!!"
